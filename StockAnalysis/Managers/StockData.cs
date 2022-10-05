6fd5640efe2e2ae8da9736d2c5f46dec69f62608
alexandergossett <alexandergossett@gmail.com>
2022-10-04 18:01:15 -0700

    changes

---
+++ StockAnalysis/Managers/StockData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using YahooFinanceApi;
+
+namespace StockAnalysis.Managers;
+
+public interface IStockData
+{
+    Task<(IReadOnlyList<Candle>?, string?)> GetStockData(string symbol, DateTime startDate, DateTime endDate);
+}
+
+public class StockData : IStockData
+{
+    public async Task<(IReadOnlyList<Candle>?, string?)> GetStockData(string symbol, DateTime startDate, DateTime endDate)
+    {
+        try
+        {
+            //var historicalDataTask = await Yahoo.GetHistoricalAsync(symbol);
+            var history = await Yahoo.GetHistoricalAsync("AAPL", new DateTime(2016, 1, 1), new DateTime(2016, 7, 1), Period.Daily);
+            var security = await Yahoo.Symbols(symbol).Fields(Field.LongName).QueryAsync();
+
+
+            var ticker = security.GetValueOrDefault(symbol);
+            var companyName = ticker?.LongName;
+
+            //var historicalStockData = await historicalDataTask;
+
+            return (history, companyName);
+        }
+        catch
+        {
+            return (null, null);
+        }
+
+    }
+}
+
