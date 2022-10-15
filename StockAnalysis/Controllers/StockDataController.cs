0776504f07e6450785638d234c5b25a30f35c8fe
alexandergossett <alexandergossett@gmail.com>
2022-10-15 15:23:04 -0700

    changes

---
+++ StockAnalysis/Controllers/StockDataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockAnalysis.Managers;
+using YahooFinance.NET;
+
+namespace StockAnalysis.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StockDataController : ControllerBase
+{
+    private readonly ILogger<StockDataController> _logger;
+    private readonly IStockData _stockData;
+
+
+    public StockDataController(ILogger<StockDataController> logger, IStockData stockData)
+    {
+        _logger = logger;
+        _stockData = stockData;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<YahooHistoricalPriceData>> Get()
+    {
+        return await _stockData.GetStockData("AAPL", DateTime.Today.AddMonths(-15), DateTime.Today);
+    }
+}
+
